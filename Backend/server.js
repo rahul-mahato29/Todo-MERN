a3fa38ae46ed4e14bb6eefb0b24d48fbb5212a38
Rahul Mahato <mahatorahul2901@gmail.com>
2024-04-17 22:32:34 +0530

    cors installed

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const TodoRoutes = require('./Routes/user');
+const cors = require('cors');
+app.use(cors({
+    origin: "http://localhost:5173"   //means only this frontend can hit the backend, for secure - if we not mention anything then any frontend can hit the backend.
+}));
 
 app.use('/todo', TodoRoutes);
 
