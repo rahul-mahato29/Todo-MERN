0c89d17017113a2ad0611452ab32fc415f737543
Rahul Mahato <mahatorahul2901@gmail.com>
2024-04-18 09:22:38 +0530

    updating database from frontend

--- Frontend/src/Component/Todo.jsx
+++ Frontend/src/Component/Todo.jsx
@@ -3,9 +3,33 @@ import TodoContent from "./TodoContent";
 
 //Todo Component
 const Todo = () => {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    //updating the database with todo details
+    function submitDetails(){
+        //use can also use "axios", its syntax is little easy
+        fetch("http://localhost:3000/todo/create", {
+            method: "POST",
+            body: JSON.stringify({
+                title: title,
+                description: description
+            }), 
+            headers: {
+                "content-Type": "application/json"
+            }
+        })
+        .then(async function(res){
+            const json = await res.json();
+            alert("Todo Added");
+        })
+    }
+
+
     const [todo, setTodo] = useState([]);
 
-    //connecting with backend
+    //connecting with backend (Reading all the todos from database)
     fetch("http://localhost:3000/todo/read").then(
         async function(res){
             const jsonData = await res.json();
@@ -18,10 +42,10 @@ const Todo = () => {
             <div className="flex justify-center">
                 <form action="" className="border-2 border-black p-3">
                     <lable className="font-semibold">Title : </lable>
-                    <input type="text" name="title"></input><br /><br />
+                    <input type="text" name="title" onChange={(e) => setTitle(e.target.value)}></input><br /><br />
                     <lable className="font-semibold">Description : </lable>
-                    <input type="text" name="description"></input><br />
-                    <button className="bg-gray-400 p-1 pl-2 pr-2 m-1 rounded-md hover:bg-gray-500 font-semibold">Submit</button>
+                    <input type="text" name="description" onChange={(e) => setDescription(e.target.value)}></input><br />
+                    <button className="bg-gray-400 p-1 pl-2 pr-2 m-1 rounded-md hover:bg-gray-500 font-semibold" onClick={submitDetails}>Submit</button>
                 </form>
 
             </div>
--- Frontend/src/Component/TodoContent.jsx
+++ Frontend/src/Component/TodoContent.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 const TodoContent = ({ todo }) => {
     console.log("Testing");
+
     return (
         <div>
             {todo.map((info) => {
@@ -7,6 +10,8 @@ const TodoContent = ({ todo }) => {
                     <h2><span className="font-semibold">Title : </span>{info.title}</h2>
                     <h2><span className="font-semibold">Description : </span>{info.description}</h2>
                     <button className="bg-gray-400 pl-1 pr-1 mt-1 rounded-md hover:bg-gray-500">{info.completed == true ? "Completed" : "Complete"}</button>
+                    <button className="bg-gray-400 p-1 pl-2 pr-2 m-1 rounded-md hover:bg-gray-500 font-semibold">Edit</button>
+                    <button className="bg-gray-400 p-1 pl-2 pr-2 m-1 rounded-md hover:bg-gray-500 font-semibold">Delete</button>
                 </div>
             })}
         </div>
